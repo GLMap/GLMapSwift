1ac1ad4547b3cddbce8bbfe320c22e0910f062d0
Evgen Bodunov <evgen@getyourmap.com>
2023-09-20 19:58:10 +0200

    Release 1.7.4

--- Package.swift
+++ Package.swift
@@ -8,16 +8,8 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "GLMapSwift",
-            targets: ["GLMapSwift", "GLMapCore"]
-        ),
-        .library(
-            name: "GLMapCore",
-            targets: ["GLMapCore"]
-        ),
-        .library(
             name: "GLMap",
-            targets: ["GLMap", "GLMapCore"]
+            targets: ["GLMap", "GLMapCore", "GLMapSwift"]
         ),
         .library(
             name: "GLSearch",
@@ -43,23 +35,23 @@ let package = Package(
         ),
         .binaryTarget(
             name: "GLMapCore",
-            url: "https://globus.software/download/GLMapCore-1.7.3.zip",
-            checksum: "3151958d8dee627622a5f13ebcaa00058ed3ccbceb8b2fec7d95c95574fa87f7"
+            url: "https://globus.software/download/GLMapCore-1.7.4.zip",
+            checksum: "b57d64d01660728b1a5adc5a1401c5d65b5beaa21025d1098f4d5621d0a4bbc4"
         ),
         .binaryTarget(
             name: "GLMap",
-            url: "https://globus.software/download/GLMap-1.7.3.zip",
-            checksum: "28bba40e2f37c4b8490c34cb09d5da1516bff74ca93ede68fcbf24ce2fd03606"
+            url: "https://globus.software/download/GLMap-1.7.4.zip",
+            checksum: "baec8cbdb5ac62969d27c22951d1069520922bdaebefd0b9d1041a2ef4084a52"
         ),
         .binaryTarget(
             name: "GLSearch",
-            url: "https://globus.software/download/GLSearch-1.7.3.zip",
-            checksum: "72418450bf15db9b3a35d9673f8c8ebbd0aa36e6d41e604c18971d1edc180122"
+            url: "https://globus.software/download/GLSearch-1.7.4.zip",
+            checksum: "23bb0b4ce15dec4e8f2b9cb525d67431ece8d1ac645f9a266a68a71ebf8cd7ad"
         ),
         .binaryTarget(
             name: "GLRoute",
-            url: "https://globus.software/download/GLRoute-1.7.3.zip",
-            checksum: "347aacf7d73e71ada4e18f99f5479a415121e0337abdcd45bc69003bc3721afc"
+            url: "https://globus.software/download/GLRoute-1.7.4.zip",
+            checksum: "a0df579dab937912a91ae300f03a67c888a328f497c2468322e5e761d26c75db"
         ),
     ]
 )
