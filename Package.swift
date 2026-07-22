ca75940c1bba4890d0e70df86145f1b93fb4d0ec
Evgen Bodunov <evgen@getyourmap.com>
2026-07-22 16:04:47 +0200

    Release 2.0

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
             name: "GLMapSwift",
             dependencies: ["GLMap", "GLMapCore"],
             path: ".",
-            exclude: ["README.md", "LICENSE.txt", "GLMapSwift.podspec"],
+            exclude: ["README.md", "LICENSE.txt", "GLMapSwift.podspec", "GLMapSwift-Info.plist"],
             sources: ["SwiftExtensions.swift"],
             resources: [
                 .copy("Resources/world.vm"),
@@ -36,23 +36,23 @@ let package = Package(
         ),
         .binaryTarget(
             name: "GLMapCore",
-            url: "https://globus.software/download/GLMapCore-1.12.0.zip",
-            checksum: "c4edf9b3757cc9fe8d02a1c6783f6f1c2c2e50d602074db1f92fd8aa56a67a54"
+            url: "https://globus.software/download/GLMapCore-2.0.0.zip",
+            checksum: "5a30d5593b5963cac06c1d1c3a1bb09d71585ff993ab9209f9a0f505fb93b313"
         ),
         .binaryTarget(
             name: "GLMap",
-            url: "https://globus.software/download/GLMap-1.12.0.zip",
-            checksum: "d977bab5085b3ab7016a30d1248f9f33a288b76cdb4c6a53480ed838dc49f3c5"
+            url: "https://globus.software/download/GLMap-2.0.0.zip",
+            checksum: "f03ef0c6d723647b229a64f41532d47d6f58c55fd27e9cba042b5425f9f73831"
         ),
         .binaryTarget(
             name: "GLSearch",
-            url: "https://globus.software/download/GLSearch-1.12.0.zip",
-            checksum: "6c72a8251da3e408bbd8e40f063db6e6c4c76a05daf57910af0303e6fbac28a3"
+            url: "https://globus.software/download/GLSearch-2.0.0.zip",
+            checksum: "aa30104eacf3ad8fb74797881d195be741057dd828a36c60e4eeca6cdba03019"
         ),
         .binaryTarget(
             name: "GLRoute",
-            url: "https://globus.software/download/GLRoute-1.12.0.zip",
-            checksum: "fe6276fa206f06b9853fbc9276f5a99756dcb35f096f58b80338034fc8fc6fbf"
+            url: "https://globus.software/download/GLRoute-2.0.0.zip",
+            checksum: "e48f5bbbbd54602862ae635648690ef447090c659a4b3b54c4690d5f52772587"
         ),
     ]
 )
